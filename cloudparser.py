3fe56ee8426c59cee2ee1582fbd9f9bf79154c8f
ovsiienkoap <ovsiienko.andrii@gmail.com>
2025-06-24 23:19:43 +0300

    Don't know, if the thing with bigquery actually works, but with initialization and setting up the IAM should be fine

--- cloudparser.py
+++ cloudparser.py
@@ -2,6 +2,7 @@ import time
 import requests
 import json
 import datetime
+from google.cloud import bigquery
 
 with open("config.json", 'r', encoding="utf-8" ) as f:
     config = json.load(f)
@@ -36,6 +37,13 @@ def get_json(start_point):
 PARSE_TIME_DELAY = config["PARSE_TIME_DELAY"]
 PARSE_ERROR_DELAY = config["PARSE_ERROR_DELAY"]
 
+PROJECT_ID = config["PROJECT_ID"]
+DATASET_ID = config["DATASET_ID"]
+TABLE_ID = config["TABLE_ID"]
+
+client = bigquery.Client(project=PROJECT_ID)
+table_ref = client.dataset(DATASET_ID).table(TABLE_ID)
+
 data = {"results": [1]}
 start_point = 0
 
@@ -52,22 +60,21 @@ while len(data['results']) != 0 and len(case_list) != 0:
         else:
             continue
 
-        timestamp = datetime.datetime.now(datetime.timezone.utc)
+        timestamp = datetime.datetime.now(datetime.timezone.utc).replace(minute=0, second=0, microsecond=0)
 
         row = {'hash_name': inst['hash_name'],
                'sell_listings': inst['sell_listings'],
-               'sell_price_text': inst['sell_price_text'],
-               'sale_price_text': inst['sale_price_text'],
-               'date': str(timestamp.date()),
-               'hour': timestamp.hour,
+               'sell_price_text': inst['sell_price_text'][1:], #because dollar sign
+               'sale_price_text': inst['sale_price_text'][1:], #because dollar sign
+               'date': timestamp #str(timestamp.date())
                }
         batch.append(row)
 
     #sending batch to the cloud todo
-    #if len(batch)==0: do not insert rows
+    # if len(batch) != 0:
+    #     with open("cloudlike.csv", "a") as f:
+    #         f.write(str(batch))
     if len(batch) != 0:
-        with open("cloudlike.csv", "a") as f:
-            f.write(json.dumps(batch, indent=4))
-
+        errors = client.insert_rows_json(table_ref, batch)
     #sending batch to the cloud todo
     time.sleep(PARSE_TIME_DELAY)
